bea8ae20f568c0cbe671b926ac170c97d632fe03
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-07-11 18:39:05 +0530

    Add post Form and Post List Component

--- redux-02/src/App.js
+++ redux-02/src/App.js
@@ -1,9 +1,11 @@
-import Counter from "./features/counter/Counter";
+import AddPostForm from "./features/posts/AddPostForm";
+import PostList from "./features/posts/PostList";
 
 function App() {
   return (
     <main className="App">
-      <Counter />
+      <AddPostForm />
+      <PostList />
     </main>
   );
 }
