af7d64deb1dd3292f109c0d98abcd14bfc8cf597
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-07-11 23:05:34 +0530

    add fetchUsers

--- redux-03/src/index.js
+++ redux-03/src/index.js
@@ -5,6 +5,10 @@ import "./index.css";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
 
+import { fetchUsers } from "./features/users/usersSlice";
+
+store.dispatch(fetchUsers());
+
 ReactDom.render(
   <React.StrictMode>
     <Provider store={store}>
